c57b8dd61ae86764fb4acf23433aa12b87f008b3
Said Gama <sgamap8@gmail.com>
2023-03-27 16:40:55 -0600

    se agregan archivos

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function part1() {
+    let listaEspera = ["Sofia", "David", "Juan"];
+    console.log(listaEspera);
+
+    listaEspera.push("Sara");
+    listaEspera.push("Agustin");
+    listaEspera.shift();
+    console.log(listaEspera);
+
+    listaEspera.splice(1,0,"Renata");
+    listaEspera.push("Elena");
+    console.log(listaEspera);
+}
+part1();
+
+function excercise2(n) {
+    for
+}
+
